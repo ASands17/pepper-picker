0a19eed84f20677fa2113780a162cf0b2a59d900
Amanda Sands <amandajean555@gmail.com>
2022-08-04 16:34:27 -0600

    Add checkbox that updates state of Pepper Cards

--- src/PepperCard.js
+++ src/PepperCard.js
@@ -1,7 +1,14 @@
 import { Link } from "react-router-dom";
 import "./PepperCard.css"
+import React, { useState } from "react";
 
 const PepperCard= (props) => {
+
+    const [checked, setChecked] = useState(false);
+
+    let handleChange= () => {
+        setChecked(!checked)
+    }
 
     return (
         <div className="pepperCard">
@@ -10,6 +17,13 @@ const PepperCard= (props) => {
             </Link>
             <img className="pepperCardImage" src={props.pepper.imageUrl} />
             <h2>Spice Level: {props.pepper.spiceLevel}</h2>
+            <label>
+                <input 
+                type="checkbox"
+                checked={checked}
+                onChange={handleChange} />
+                Add to My Peppers
+                </label>
         </div>
     )
 }
