66c13a761f293c891212d8b6f6c071c6b6138d85
Amanda Sands <amandajean555@gmail.com>
2022-08-04 08:55:10 -0600

    Impliment fetch call from API

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
-import React, { FC, useState } from "react";
+import React, { FC, useEffect, useState } from "react";
 import AllPeppers from "./AllPeppers"
 import PepperDetails from "./PepperDetails"
 
 const App= () => {
+
+  const [peppers, setPeppers] = useState([]);
+  const [error, setError] = useState('');
+  
+  const getPeppers = async () => {
+    try {
+    const response = await fetch("https://polar-inlet-62371.herokuapp.com/peppers");
+    const allPeppers = await response.json();
+    setPeppers(allPeppers);
+  } catch (error) {
+    setError(
+      "Sorry, there has been a problem loading your page. Try again!"
+      );
+  }
+}
+
+useEffect(() => {
+  getPeppers();
+}, []);
+
+
   return(
     <div>
       <h1>FULL APP RENDER</h1>
-    <AllPeppers />
+    <AllPeppers pepperPreview={peppers} error={error}/>
     <PepperDetails />
     </div>
   )
