1c5ad4f686f3fc38d26412abcc09b65d67115eeb
Amanda Sands <amandajean555@gmail.com>
2022-08-04 10:50:59 -0600

    Add placeholder text for MyPepper component

---
+++ src/MyPeppers.js
@@ -0,0 +1,9 @@
+import React, { FC, useState } from "react";
+
+const MyPeppers=() => {
+    return (
+        <h1> You don't have any peppers selected right now. Go pick a pepper! </h1>
+    )
+}
+
+export default MyPeppers;
